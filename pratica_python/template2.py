5880764dc4dca42130d1c0400bd80cb45232110d
Luan Dev <luansales.oficial@gmail.com>
2024-09-09 09:52:39 -0300

    prática com for.

---
+++ pratica_python/template2.py
@@ -0,0 +1,16 @@
+valor1 = int(input('Digite o valor correspondente a tabuada desejada: '))
+# Tabuada de adição 
+print('Tabuada de adição')
+print('')
+for i in range(1, 11):
+    print(f'{valor1} + {i} = {valor1 + i}')
+print('')
+
+valor1 = int(input('Digite o valor correspondente a tabuada desejada: '))
+# Tabuada de subtração
+print('Tabuada de subtração')
+print('Tabuada de adição')
+print('')
+for i in range(1, 11):
+    print(f'{valor1} - {i} = {valor1 - i}')
+print('')
